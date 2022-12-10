a1ce5a25601cda24889c414e0016579a7f82d56c
Ayush <ayushgandhi3105@gmail.com>
2022-12-10 23:05:06 +0530

    setup project

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,12 @@
 const express=require('express');
 
+const PORT=process.env.PORT || 5000
 const app=express();
-app.listen('5000',(req,res)=>{
-    console.log();
+
+//route
+app.get("/",(req,res)=>{
+    res.send("<h2> home page</h2>")
+})
+app.listen(PORT,(req,res)=>{
+    console.log(`Sever running on PORT no ${PORT}`);
 })
